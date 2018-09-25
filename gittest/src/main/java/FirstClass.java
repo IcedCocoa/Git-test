fa91a47df38534b95bb459f4ed6ee8f27368f3b4
JD <JD@itcast.com>
2018-09-25 18:53:27 +0800

    把本地工程添加到Git

---
+++ gittest/src/main/java/FirstClass.java
@@ -0,0 +1,15 @@
+/**
+ * @author Sun
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
